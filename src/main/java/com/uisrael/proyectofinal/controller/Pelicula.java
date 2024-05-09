c5a61b5bdd3667655c07dcfbef1e52ff8b7bc7d4
Master <master@srvuisrael.edu.ec>
2024-05-08 21:55:39 -0500

    index page

--- src/main/java/com/uisrael/proyectofinal/controller/Pelicula.java
+++ src/main/java/com/uisrael/proyectofinal/controller/Pelicula.java
@@ -1,5 +1,15 @@
 package com.uisrael.proyectofinal.controller;
 
-public class Pelicula {
+import org.springframework.web.bind.annotation.GetMapping;
 
+public class Pelicula {
+	@GetMapping
+	public String home() {
+		return "redirect:/index";
+	}
+	
+	@GetMapping("/index")
+	public String listarReservas(){
+		return "index";
+	}
 }
